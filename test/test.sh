cf75a92fe552e9103561b00bac689320f5adb47f
Simon Will <will@cl.uni-heidelberg.de>
2016-12-02 02:45:22 +0100

    Ensure that pip3 is available.

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,6 @@
 MIN_PYLINT_RATING=${1:-7}
 
 DIR=$(dirname "${BASH_SOURCE[0]}")
-pip install -r "$DIR/requirements.test.txt"
 
 echo 'Assessing code quality with pylint ...'
 pylint --rcfile="$DIR/pylintrc" "$DIR/$PROJECT" >pylint_results || true
@@ -13,5 +12,8 @@ ACTUAL_PYLINT_RATING=$(grep -Po '(?<=Your code has been rated at )[^/]+(?=/10)' 
 echo "Minimal acceptable pylint rating is ${MIN_PYLINT_RATING}."
 echo "Actual pylint rating is ${ACTUAL_PYLINT_RATING}."
 
-[ -z "$ACTUAL_PYLINT_RATING" ] && echo "Pylint error: $(cat pylint_results)"
-python -c "import sys; sys.exit(0) if $ACTUAL_PYLINT_RATING >= $MIN_PYLINT_RATING else sys.exit(1)"
+if [ -z "$ACTUAL_PYLINT_RATING" ]; then
+    echo "Pylint error: $(cat pylint_results)"
+else
+    python -c "import sys; sys.exit(0) if $ACTUAL_PYLINT_RATING >= $MIN_PYLINT_RATING else sys.exit(1)"
+fi
