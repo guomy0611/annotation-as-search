8416f383823727b646001e811ca4e853e6361a75
hubert <hubert@cl.uni-heidelberg.de>
2016-12-27 00:28:03 +0100

    finished surface, annotation process to be added

--- annotation-helper-client/gui/__init__.py
+++ annotation-helper-client/gui/__init__.py
@@ -1,33 +1,85 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect, send_file
+from werkzeug.utils import secure_filename
+import sys
+import os
+from conll_convert import conll06_to_conll09
 
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(['conll', 'conll09', 'conll06', 'txt'])
+UPLOAD_FOLDER = 'loadedFiles'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'jqUNf8?B\8d&(teVZq,~'
 
 @app.route('/')
 def homepage():
     return render_template("index.html")
 
-#@app.route('/annotate')
-#def annotate():
-    # TODO get server data
-#    html_tree = visualizer.visualize_solution(args, 0)
-#    return render_template(visualize.py(html_tree))
+@app.route('/annotate')
+def annotate():
+   # TODO get server data
+   render_template("annotate.html")
 
-@app.route('/exit/')
+@app.route('/exit/', methods = ["POST", "GET"])
 def close_annotator():
+    if request.method == "POST":
+        if request.form["closeApplication"]:
+            # ugly, but functional, improve later
+            print("Ending Annotation Helper")
+            sys.exit()
     return render_template('exit.html')
 
-@app.route('/loadFile/')
-def loadFile():
-    pass
 
-@app.route('/saveFile/')
+@app.route('/save_file/', methods = ["POST", "GET"])
 def saveFile():
-    pass
+    if request.method == "POST":
+        if request.form["saveFile"]:
+            return send_file(
+                "templates/visualized_tree.html",
+                 attachment_filename="annotated_sentence.conll09"
+                 )
 
-@app.route('/endResult/')
+    return render_template("save_file.html")
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.split('.')[-1].lower() in  ALLOWED_EXTENSIONS
+
+@app.route('/load_file/', methods = ["POST", "GET"])
+def loadFile():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flash('No file')
+            return redirect(url_for('homepage'))
+        data_file = request.files['file']
+        if data_file.filename == '':
+            flash('No file selected')
+            return redirect(url_for('loadFile'))
+
+        print(data_file.filename)
+        if allowed_file(data_file.filename):
+            data = secure_filename(data_file.filename)
+            data_file.save(os.path.join(app.config['UPLOAD_FOLDER'], data))
+            return redirect(url_for('annotate', filename=data))
+
+    return render_template("load_file.html")
+
+@app.route('/endResult/', methods=["GET", "POST"])
 def annotation_finished():
-    #final = visualizer.visualize_solution(finishedTree, 1)
-    return render_template("visualized_tree.html")
+    try:
+        if request.method == "POST":
+            answer = request.form["answer"]
+            if answer == "Yes":
+                # TODO get conll part, convert to conll09 and save in static
+                return redirect(url_for('saveFile'))
+            else:
+                return render_template("visualized_tree.html",
+                                        message="Please correct the tree")
+
+        else:
+            return render_template("visualized_tree.html")
+    # Debugging, remove later
+    except Exception as e:
+        return render_template("visualized_tree.html", message=e)
 
 @app.errorhandler(404)
 def page_not_found(e):
